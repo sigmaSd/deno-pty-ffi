ba537a5565555ee96c5b5219872be6a66e15c277
Nbiba Bedis <bedisnbiba@gmail.com>
2023-09-01 21:17:45 +0100

    chore: bump rust lib

--- src/mod.ts
+++ src/mod.ts
@@ -53,7 +53,7 @@ export class Pty {
     // Tag version with the prebuilt lib
     // It doesn't have to be the same as the library version
     // Only update it when the rust library gets updated
-    const version = "0.13.0";
+    const version = "0.14.0";
     const url =
       `https://github.com/sigmaSd/deno-pty-ffi/releases/download/${version}`;
 
