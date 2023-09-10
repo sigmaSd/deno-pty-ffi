5a3a3bc49b3c6df6321676075bbbc33510c499d1
Nbiba Bedis <bedisnbiba@gmail.com>
2023-09-10 08:24:51 +0100

    fix test

--- c.ts
+++ c.ts
@@ -37,7 +37,7 @@ if (import.meta.main) {
   // const cmd = Deno.build.os !== "windows" ? "irust" : "irust.exe";
   const pty = await Pty.create({
     // cmd: "./irust-irust@1.71.2-x86_64-pc-windows-msvc/irust.exe",
-    cmd: "./IRust/target/debug/irust.exe",
+    cmd: "./IRust/target/release/irust.exe",
     args: ["--default-config"],
     env: [["NO_COLOR", "1"]],
   });
