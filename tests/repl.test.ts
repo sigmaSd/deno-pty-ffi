febe1ac57cf5cf1cb61162d9a5275aca0d95fb02
Nbiba Bedis <bedisnbiba@gmail.com>
2023-10-18 18:18:01 +0100

    update dep

--- tests/repl.test.ts
+++ tests/repl.test.ts
@@ -1,4 +1,4 @@
-import { assert } from "https://deno.land/std@0.203.0/testing/asserts.ts";
+import { assert } from "https://deno.land/std@0.203.0/assert/assert.ts";
 import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
 import { Pty } from "../mod.ts";
 
