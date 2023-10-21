f59ddf5c054db5d450fc56eac5b316ebd46106f3
Nbiba Bedis <bedisnbiba@gmail.com>
2023-10-21 19:54:20 +0100

    try fix windows

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,7 @@
 import { plug } from "./deps.ts";
 import {
   decode_cstring,
+  decode_json_cstring,
   encode_cstring,
   encode_json_cstring,
 } from "./utils.ts";
@@ -9,6 +10,20 @@ export interface Command {
   cmd: string;
   args: string[];
   env: [string, string][];
+}
+
+/** Represents the size of the visible display area in the pty */
+export interface PtySize {
+  /** The number of lines of text*/
+  rows: number;
+  /** The number of columns of text*/
+  cols: number;
+  /** The width of a cell in pixels.  Note that some systems never
+  fill this value and ignore it.*/
+  pixel_width: number;
+  /** The height of a cell in pixels.  Note that some systems never
+  fill this value and ignore it.*/
+  pixel_height: number;
 }
 
 interface PtyApi extends Deno.ForeignLibraryInterface {
@@ -141,6 +156,29 @@ export class Pty {
     const result = await this.#lib.symbols.pty_write(
       this.#this,
       encode_cstring(data),
+      errBuf,
+    );
+    const ptr = Deno.UnsafePointer.create(
+      new BigUint64Array(errBuf.buffer)[0],
+    )!;
+    if (result === -1) throw new Error(decode_cstring(ptr));
+  }
+
+  getSize(): PtySize {
+    const dataBuf = new Uint8Array(8);
+    const result = this.#lib.symbols.pty_get_size(this.#this, dataBuf);
+    const ptr = Deno.UnsafePointer.create(
+      new BigUint64Array(dataBuf.buffer)[0],
+    )!;
+    if (result === -1) throw new Error(decode_cstring(ptr));
+    return decode_json_cstring(ptr);
+  }
+
+  resize(size: PtySize) {
+    const errBuf = new Uint8Array(8);
+    const result = this.#lib.symbols.pty_resize(
+      this.#this,
+      encode_json_cstring(size),
       errBuf,
     );
     const ptr = Deno.UnsafePointer.create(
